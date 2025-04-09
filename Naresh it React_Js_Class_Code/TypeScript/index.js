344aaf05363b57a5de88fa4d668ebce0e3041318
RitikAnand02 <ritikanand.02062000@gmail.com>
2025-04-09 22:37:40 +0530

    new

--- Naresh it React_Js_Class_Code/TypeScript/index.js
+++ Naresh it React_Js_Class_Code/TypeScript/index.js
@@ -1,1 +1,11 @@
-console.log("Welcome to TypeScript");
+var FetchData = new Promise(function (resolve, reject) {
+    var url = "https://server.com&quot;;;
+    if (url === "https://fakestore.com&quot;){)
+        resolve({ Name: 'TV', Price: 50000 });
+}, {});
+FetchData.then(function (data) {
+    console.log(data);
+})
+    .catch(function (error) {
+    console.log(error);
+});
--- Naresh it React_Js_Class_Code/TypeScript/index.ts
+++ Naresh it React_Js_Class_Code/TypeScript/index.ts
@@ -1,1 +1,18 @@
-console.log("Welcome to TypeScript");
+
+let FetchData:Promise<{Name:string, Price:number}> = new Promise(function(resolve, reject){
+       
+    let url = "https://server.com&quot;;
+
+    if(url==="https://fakestore.com&quot;){
+         resolve({Name:'TV', Price:50000});
+    } else {
+         reject('Invalid URL - API Not found');
+    }
+})
+
+FetchData.then(function(data){
+console.log(data);
+})
+.catch(function(error){
+console.log(error);
+})
